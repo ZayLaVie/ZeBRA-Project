62caf6cc1a2a4b5d55e3ccfe180e4aba56569ffe
asamsomb <aidaa.lee@gmail.com>
2023-11-15 15:49:53 -0500

    Add Forum List HTML

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return render_template('create_account.html')
-
-@app.route('/login')
-def login():
     return render_template('login.html')
+
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
